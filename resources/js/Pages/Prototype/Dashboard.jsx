5b5e539d4ee4f70cde10108f4adb990e6ee7b845
DESKTOP-EKMQPC3\Rian <ryancahyadi11@gmail.com>
2024-02-20 22:49:33 +0700

    feat: slicing featured & movie card

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -2,9 +2,10 @@ import InputLabel from "@/Components/InputLabel";
 import TextInput from "@/Components/TextInput";
 import PrimaryButton from "@/Components/PrimaryButton";
 import { Link, Head } from "@inertiajs/react";
-
+import FeaturedMovies from "@/Components/FeaturedMovies";
 import Authenticated from "@/Layouts/Authenticated/Index";
 import Flickity from "react-flickity-component";
+import MovieCard from "@/Components/MovieCard";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -19,59 +20,49 @@ export default function Dashboard() {
 
     return (
         <Authenticated>
-            <Head>                
-                <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css" />
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
             </Head>
             <div>
                 <div className="font-semibold text-[22px] text-black mb-4">
                     Featured Movies
                 </div>
-                <Flickity className="gap-[30px]" options={flickityOptions}>
+                <Flickity className="gap-[30px] mb-4" options={flickityOptions}>
                     {[1, 2, 3, 4].map((i) => (
                         // {/* <!-- Movie Thumbnail --> */}
-                        <div className="absolute overflow-hidden group mr-[30px]" key={i}>
-                            <img
-                                src="/images/featured-1.png"
-                                className="object-cover rounded-[30px] w-[520px] h-[340px]"
-                                alt=""
-                            />
-                            {/* <!-- rating --> */}
-                            <div className="rating absolute top-0 left-0">
-                                <div className="p-[30px] flex items-center gap-1">
-                                    <img src="/icons/ic_star.svg" alt="" />
-                                    <span className="text-sm font-medium text-white mt-1">
-                                        4.5/5.0
-                                    </span>
-                                </div>
-                            </div>
-                            {/* <!-- bottom detail --> */}
-                            <div
-                                className="absolute bottom-0 h-[100px] left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
-                            rounded-br-[28px] flex justify-between items-center px-7 h-[130px]"
-                            >
-                                <div>
-                                    <div className="font-medium text-[22px] text-white">
-                                        The Batman in Love
-                                    </div>
-                                    <p className="mb-0 text-white text-sm font-light">
-                                        Action • Horror
-                                    </p>
-                                </div>
-                                <div className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
-                                    <img
-                                        src="/icons/ic_play.svg"
-                                        width="50"
-                                        alt=""
-                                    />
-                                </div>
-                            </div>
-                            <a
-                                href="watching.html"
-                                className="inset-0 absolute z-50"
-                            ></a>
-                        </div>
+                        <FeaturedMovies
+                            key={i}
+                            slug="the-batman-in-love"
+                            thumbnail="/images/featured-1.png"
+                            rating={i + 1}
+                            category="Action • Horror"
+                            name={`The Batman in Love ${i}`}
+                        />
                     ))}
                 </Flickity>
+                <div>
+                    <div className="font-semibold text-[22px] text-black mb-4 mt-14">
+                        Browse
+                    </div>
+                    <Flickity
+                        className="__scroll-selector"
+                        options={flickityOptions}
+                    >
+                        {/* <!-- Movies 1 --> */}
+                        {[1, 2, 3, 4, 5, 6].map((i) => (
+                            <MovieCard
+                                key={i}
+                                slug="meong-golden"
+                                thumbnail="/images/browse-1.png"
+                                category="Horror • Love"
+                                name={`Meong Golden ${i}`}
+                            />
+                        ))}
+                    </Flickity>
+                </div>
             </div>
         </Authenticated>
     );
